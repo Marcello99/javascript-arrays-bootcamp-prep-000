23e6c0a821a5ee22a6ee606558f257b4eeb01033
Marcello Lindo <marcellolindo34@gmail.com>
2018-07-02 19:45:49 +0000

    automatically backed up by learn

--- test/arrays-test.js
+++ test/arrays-test.js
@@ -12,11 +12,11 @@ describe('arrays', () => {
 
   describe('chocolateBars', () => {
     it('is an array containing "snickers", "hundred grand", "kitkat", and "skittles"', () => {
-      expect(chocolateBars).to.eql['snickers', 'hundred grand', 'kitkat', 'skittles'];
+      expect(chocolateBars).to.eql['snickers', 'hundred grand', 'kitkat', 'skittles']
     });
   });
-
-  describe('addElementToBeginningOfArray(array, element)', () => {
+var chocolateBars = ["snickers", "hundred grand", "kitkat", and "skittles"]
+  describe;('addElementToBeginningOfArray(array, element)', () => {
     it('adds an element to the beginning of an array', () => {
       expect(addElementToBeginningOfArray([1], 'foo')).to.eql(['foo', 1]);
     });
